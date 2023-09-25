7789127a244a71c5f9c3069efd4e92b31c74d7e1
Zenric Navea <jazx.zn@gmail.com>
2023-09-26 12:30:46 +1300

    Added appium configuration

--- TSTest.ts
+++ TSTest.ts
@@ -1,5 +1,27 @@
-const date = new Date();
+console.log(process.argv);
+console.log(process.cwd());
+console.log(__dirname);
 
-console.log(date.getUTCDate());
-console.log(date.toTimeString());
-console.log(date.toLocaleTimeString());
+function reqVariableChecker(variables: string[]) {
+
+    let reqVar: boolean;
+    variables.forEach(variable => {
+        for (const envVar of process.argv) {
+            reqVar = false;
+            envVar.includes(`${variable}=`) ? reqVar = true : reqVar = false;
+            if (reqVar) {
+                break;
+            }
+        }
+        if (!reqVar) {
+            console.log(`${variable.toUpperCase()} was not declared. Please add in the command line, e.g. ${variable}=foo`);
+            process.exit(1);
+        }
+    });
+}
+
+let requiredVariables = [
+    "env"
+]
+
+reqVariableChecker(requiredVariables);
